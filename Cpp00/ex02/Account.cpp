7abe78fac95fda7cc0e8e30a051046ef01fbfd86
sielee <lalala97557@gmail.com>
2022-12-19 11:16:35 +0900

    cpp00 ex02

--- Cpp00/ex02/Account.cpp
+++ Cpp00/ex02/Account.cpp
@@ -1,5 +1,6 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
 
 Account::Account()
 {
@@ -26,4 +27,60 @@ Account::~Account()
 	std::cout << "closed\n";
 	_totalAmount -= _amount;
 	_nbAccounts--;
+}
+
+int	Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits()
+{
+	return (_nbDeposits);
+}
+
+int	Account::getNbWithdrawals()
+{
+	return (_nbWithdrawals);
+}
+
+void	Account::makeDeposit(int deposit)
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount() const
+{
+
+}
+
+void	Account::displayStatus() const
+{
+
+}
+
+void	Account::displayAccountsInfos()
+{
+	std::cout << "accounts:" << getNbAccounts() << ';';
+	std::cout << "total:" << getTotalAmount() << ';';
+	std::cout << "deposits:" << getNbDeposits() << ';';
+	std::cout << "withdrawals:" << getNbWithdrawals() << '\n';
+}
+
+void Account::_displayTimestamp()
+{
+	const time_t	timeStamp = time(NULL);
+	char	buf[16];
+
+	strftime(buf, sizeof(buf), "%Y%m%d_%H%M%S", localtime(&timeStamp));
 }
