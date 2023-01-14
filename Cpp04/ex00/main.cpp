f249f68d46233bad7918c689abb5c8728ce78580
sielee <lalala97557@gmail.com>
2023-01-14 16:50:49 +0900

    [ADD]Wrong

--- Cpp04/ex00/main.cpp
+++ Cpp04/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 #include <iostream>
 
 int	main()
@@ -8,16 +10,19 @@ int	main()
 	const Animal*	meta = new Animal();
 	const Animal*	dog = new Dog();
 	const Animal*	cat = new Cat();
+	const WrongAnimal* wrong = new WrongCat();
+
 
 	meta->makeSound();
 	std::cout << dog->getType() << std::endl;
 	dog->makeSound();	
 	std::cout << cat->getType() << std::endl;
 	cat->makeSound();
-
+	wrong->makeSound();
 	delete meta;
 	delete dog;
 	delete cat;
+	delete wrong;
 //	system("Leaks ex00.exe");
 	return (0);
 }
