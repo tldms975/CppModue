13560621e2b77c98bceac4ca19a588d8d41e346b
sielee <lalala97557@gmail.com>
2023-01-14 19:24:49 +0900

    [ADD]test

--- Cpp04/ex00/main.cpp
+++ Cpp04/ex00/main.cpp
@@ -7,22 +7,34 @@
 
 int	main()
 {
-	const Animal*	meta = new Animal();
-	const Animal*	dog = new Dog();
-	const Animal*	cat = new Cat();
-	const WrongAnimal* wrong = new WrongCat();
+	{
+		const Animal*	meta = new Animal();
+		const Animal*	dog = new Dog();
+		const Animal*	cat = new Cat();
+		const WrongAnimal* wrong = new WrongCat();
 
 
-	meta->makeSound();
-	std::cout << dog->getType() << std::endl;
-	dog->makeSound();	
-	std::cout << cat->getType() << std::endl;
-	cat->makeSound();
-	wrong->makeSound();
-	delete meta;
-	delete dog;
-	delete cat;
-	delete wrong;
-//	system("Leaks ex00.exe");
+		meta->makeSound();
+		std::cout << dog->getType() << std::endl;
+		dog->makeSound();	
+		std::cout << cat->getType() << std::endl;
+		cat->makeSound();
+		wrong->makeSound();
+		delete meta;
+		delete dog;
+		delete cat;
+		delete wrong;
+	}
+	{
+		Cat c1;
+		Cat c2(c1);
+		if (&c1 == &c2)
+			std::cout << "same\n";
+		else
+			std::cout << "different\n";
+	}
+
+
+	//system("Leaks ex00.exe");
 	return (0);
 }
